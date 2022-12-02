7b12adf4458222edf070f54a287316bfc9348da6
sweshelo <sweshelo@gmail.com>
2022-12-03 05:24:00 +0900

    [feat] 音声認識を実装

--- client.js
+++ client.js
@@ -7,10 +7,18 @@ sock.addEventListener('open',function(e){
 });
 
 sock.addEventListener('message', (e)=>{
+  if( e.data.includes('[rec]')){
+    recognition.start();
+  }else{
+    send(e.data)
+    msg = e.data
+  }
+  /*
   e.data.text().then((text)=>{
     send(text);
     msg = text;
   })
+  */
 });
 
 var add= {}
@@ -19,11 +27,30 @@ var mo = new MutationObserver((rec, obs) => {
   for(const mut of rec){
     console.log(mut.addedNodes)
     mut.addedNodes.forEach((m)=>{
+      if ( add.innerText != m.innerText && m.innerText.trim().length > 0 ) sock.send('[terminate]')
       add = m
     })
     if ( add.innerText != msg ) sock.send(add.innerText)
   }
 })
+
+SpeechRecognition = webkitSpeechRecognition || SpeechRecognition;
+let recognition = new SpeechRecognition();
+
+recognition.lang = 'ja-JP';
+recognition.interimResults = true;
+recognition.continuous = true;
+
+let finalTranscript = ''; // 確定した(黒の)認識結果
+
+recognition.onresult = (event) => {
+  for (let i = event.resultIndex; i < event.results.length; i++) {
+    let transcript = event.results[i][0].transcript;
+    if (event.results[i].isFinal) {
+      send(transcript);
+    }
+  }
+}
 
 mo.observe(thread, {
   childList: true,
